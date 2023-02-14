07f75a45fc1b9b0a7b2e4346399244d446fb8125
lendriti1 <lendriti12345@gmail.com>
2023-02-14 12:46:22 +0100

    dadaad

--- parts/tourism/T-Restaurants.php
+++ parts/tourism/T-Restaurants.php
@@ -9,6 +9,287 @@
     <link rel="stylesheet" href="t.css" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+        .cls1 {
+            height: 100%;
+        }
+
+        .imgcrs {
+            border: 5px solid read;
+            height: 100%;
+            display: flex;
+            align-items: center;
+        }
+
+        .imgcrs1 {
+            border: 5px solid blacka;
+            height: 100%;
+        }
+
+        .imgcrs1 img {
+            border: 5px solid bluea;
+            height: 100%;
+        }
+
+        .t-res {
+            border: 2px solid red;
+            width: 100%;
+            height: 400px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .t-res1 {
+            border: 5px solid reda;
+            width: 65%;
+            height: 80%;
+            display: flex;
+            flex-direction: row;
+            background-color: rgb(101, 71, 71);
+        }
+
+        .t-resp1 {
+            border: 5px solid bluea;
+            width: 55%;
+            height: 100%;
+        }
+
+        .t-resp2 {
+            border: 5px solid reda;
+            width: 45%;
+            height: 100%;
+        }
+
+        .t-res-title {
+            border: 2px solid apurple;
+            width: 100%;
+            /* width: 60%;  per me lan pak me padding left*/
+            height: 20%;
+            display: flex;
+            align-items: center;
+            font-size: 38px;
+            justify-content: end;
+            color: rgb(254, 190, 152);
+        }
+
+        .t-res-title1 {
+            width: 94.5%;
+        }
+
+        .info-res {
+            border: 2px solid purplea;
+            width: 100%;
+            height: 15%;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+        }
+
+        .info-res1 {
+            border: 2px solid blacka;
+            width: 10%;
+            height: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .font-logo1 {
+            font-size: 20px;
+            color: rgb(254, 190, 152);
+        }
+
+        .info-res2 {
+            border: 2px solid blacka;
+            width: 85%;
+            height: 100%;
+            display: flex;
+            justify-content: start;
+            align-items: center;
+            font-size: 20px;
+            color: rgb(254, 190, 152);
+        }
+
+        .t-res-more {
+            border: 2px solid yellowgareen;
+            width: 37%;
+            height: 20%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .btn-more-res {
+            border: 2px solid blaack;
+            background-color: rgb(254, 190, 152);
+            width: 70%;
+            height: 60%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 10px;
+            font-weight: bold;
+        }
+
+        @media screen and (min-width: 875px) and (max-width: 1200px) {
+            .t-res1 {
+                width: 80%;
+            }
+
+            .btn-more-res {
+                width: 30%;
+            }
+
+            .info-res2 {
+                font-size: 16px;
+            }
+
+            .info-res1 {
+                width: 20%;
+            }
+
+            .t-res-title {
+                font-size: 30px;
+            }
+
+            .t-res-more {
+                width: 46%;
+            }
+
+            .btn-more-res {
+                width: 71%;
+            }
+        }
+
+        @media screen and (min-width: 701px) and (max-width: 875px) {
+            .t-res1 {
+                width: 80%;
+            }
+
+            .btn-more-res {
+                width: 72%;
+            }
+
+            .info-res2 {
+                font-size: 15px;
+            }
+
+            .info-res1 {
+                width: 20%;
+            }
+
+            .t-res-title {
+                font-size: 30px;
+            }
+
+            .t-res-more {
+                width: 52%;
+            }
+        }
+
+        @media screen and (min-width: 351px) and (max-width: 700px) {
+            body {
+                background-color: aqua;
+            }
+
+            .t-res {
+                flex-direction: column;
+                height: 700px;
+            }
+
+            .t-res1 {
+                flex-direction: column;
+                width: 92%;
+            }
+
+            .t-res-title {
+                font-size: 32px;
+            }
+
+            .t-resp2 {
+                width: 100%;
+            }
+
+            .t-resp1 {
+                width: 100%;
+            }
+
+            .btn-more-res {
+                width: 78%;
+                height: 60%;
+            }
+
+            .info-res1 {
+                width: 15%;
+            }
+
+            .info-res2 {
+                font-size: 17px;
+            }
+
+            .t-res-more {
+                justify-content: center;
+            }
+
+            .t-res1 {
+                height: 95%;
+            }
+
+            .t-res-more {
+                width: 45%;
+            }
+        }
+
+        @media screen and (min-width: 201px) and (max-width: 350px) {
+            .t-res {
+                flex-direction: column;
+                height: 600px;
+            }
+
+            .t-res1 {
+                flex-direction: column;
+                width: 100%;
+            }
+
+            .t-resp2 {
+                width: 100%;
+            }
+
+            .t-resp1 {
+                width: 100%;
+            }
+
+            .btn-more-res {
+                width: 60%;
+                height: 70%;
+            }
+
+            .info-res1 {
+                width: 15%;
+            }
+
+            .t-res-more {
+                justify-content: center;
+            }
+
+            .t-res1 {
+                height: 95%;
+            }
+
+            .t-res-title {
+                font-size: 22px;
+            }
+
+            .info-res2 {
+                font-size: 15px;
+            }
+
+            .btn-more-res {
+                font-size: 10px;
+            }
+        }
+    </style>
 </head>
 
 <body>
