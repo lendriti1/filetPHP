7bebd17daf805f74b51f2f2c53bed6ba1719a9e5
lendriti1 <lendriti12345@gmail.com>
2023-02-25 17:34:07 +0100

    adsad

--- parts/tourism/tourismMain.php
+++ parts/tourism/tourismMain.php
@@ -213,7 +213,7 @@
 						<figcaption class="card__caption">
 							<h2 class="card__title">NASA Has Found Hundreds Of Potential New Planets</h2>
 							<p class="card__snippet">NASA released a list of 219 new “planet candidates” discovered by the Kepler space telescope, 10 of which are similar to Earth’s size and may be habitable by other life forms.</p>
-							<a href="" class="card__button">Read more</a>
+							<a href="../Fshatrat/Vraniqi.html" class="card__button">Read more</a>
 						</figcaption>
 					</figure>
 				</div>
